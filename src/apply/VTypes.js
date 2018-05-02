d6e923187709ce6a1beb2c324a20403be7d75cc8
liuquan@tzit.cn <*weqjkK>
2018-05-02 13:29:08 +0800

    vtype添加password二次确认

---
+++ src/apply/VTypes.js
@@ -0,0 +1,10 @@
+Ext.apply(Ext.form.VTypes, {
+    password : function(val, field) {
+        if (field.initialPassField) {
+            var pwd = Ext.getCmp(field.initialPassField);
+            return (val == pwd.getValue());
+        }
+        return true;
+    },
+    passwordText : '两次输入的密码不一致,请重新输入!'
+});
